eb18ae5fd3d9830567e4b7feda1923b13ecd2e4b
hoxas <hoxas@live.com>
2022-08-06 21:26:49 -0300

    Next lessons

--- pizzajoint/src/components/Header.js
+++ pizzajoint/src/components/Header.js
@@ -2,24 +2,60 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const svgVariants = {
+  initial: { rotate: -180 },
+  final: { rotate: 0, transition: { duration: 1 } },
+};
+
+const pathVariants = {
+  initial: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  final: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
 const Header = () => {
   return (
     <header>
-      <Link to={`/`}>
-        <div className="logo">
-          <svg
-            className="pizza-svg"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 100"
-          >
-            <path
-              fill="none"
-              d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-            />
-            <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
-          </svg>
-        </div>
-      </Link>
+      <motion.div
+        className="logo"
+        drag
+        dragConstraints={{ left: 0, top: 0, right: 0, bottom: 50 }}
+        dragElastic={2}
+      >
+        <motion.svg
+          className="pizza-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          variants={svgVariants}
+          initial="initial"
+          animate="final"
+        >
+          <motion.path
+            fill="none"
+            d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            variants={pathVariants}
+            initial="initial"
+            animate="final"
+          />
+          <motion.path
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+            variants={pathVariants}
+            initial="initial"
+            animate="final"
+          />
+        </motion.svg>
+      </motion.div>
+
       <motion.div
         className="title"
         initial={{ y: -250 }}
